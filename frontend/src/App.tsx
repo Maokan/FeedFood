09de585d22352c80ca85baf5c885dd5e5af7a580
Léa Speter <106237512+Maokan@users.noreply.github.com>
2026-09-09 13:13:19 +0200

    Update Website Routes
    Update Routes to access to login page and register page from url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
 import FeedPage from './features/Feed/FeedPage';
+import { Routes, Route } from 'react-router-dom';
+import RegisterPage from './features/Auth/RegisterPage';
+import LoginPage from './features/Auth/LoginPage';
+import ProfilePage from './features/Profile/ProfilePage';
+import RequireAuth from './features/Auth/RequireAuth';
 
 function App() {
-  return <FeedPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<FeedPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/profile"
+        element={
+          <RequireAuth>
+            <ProfilePage />
+          </RequireAuth>
+        }
+      />
+    </Routes>
+  )
 }
 
 export default App;
